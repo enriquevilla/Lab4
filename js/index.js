0814f31f1426b270d763256128a1c2c911019da4
Enrique Villa <enrique.villa97@hotmail.com>
2020-03-11 11:58:15 -0600

    Added keyboard support and RegEx validation

--- js/index.js
+++ js/index.js
@@ -1,62 +1,87 @@
 let logInfo = "";
 let operation = [];
 let index = 0;
+
+function handleOpClick(event) {
+    event.preventDefault();
+    //regex to check valid numbers
+    if (/[0-9]*[.]?[0-9]+/.test(Number(document.getElementsByClassName("inputNumber")[0].value))
+    && document.getElementsByClassName("inputNumber")[0].value != "") {
+        operation[index++] = document.getElementsByClassName("inputNumber")[0].value;
+        operation[index++] = event.target.innerText;
+    } else {
+        alert("Enter a valid number");
+    }
+    document.getElementsByClassName("inputNumber")[0].value = "";
+}
+
 document.getElementsByClassName("resetButton")[0].addEventListener("click", (event) => {
     event.preventDefault();
     document.getElementsByClassName("inputNumber")[0].value = "";
     document.getElementById("resultValue").value = "";
-    document.getElementsByClassName("inputNumber")[0].focus();
 });
+
 document.getElementsByClassName("equalButton")[0].addEventListener("click", (event) => {
     event.preventDefault();
-    if (document.getElementsByClassName("inputNumber")[0].value != "") {
+    //regex to check valid numbers
+    if (/[0-9]*[.]?[0-9]+/.test(Number(document.getElementsByClassName("inputNumber")[0].value))
+            && document.getElementsByClassName("inputNumber")[0].value != "") {
         operation[index] = document.getElementsByClassName("inputNumber")[0].value;
-        document.getElementsByClassName("inputNumber")[0].value = "";
         operation = operation.join(" ");
-        logInfo = operation;
-        logInfo += " = ";
-        logInfo += eval(operation);
+        logInfo = `${operation} = ${eval(operation)}`;
         document.getElementById("resultValue").value = eval(operation);
         document.getElementById("logInformation").value += `${logInfo}\n`
         index = 0;
-        operation = [];
-        document.getElementsByClassName("inputNumber")[0].focus();
+        operation = [];    
+    } else {
+        alert("Enter a valid number");
     }
+    document.getElementsByClassName("inputNumber")[0].value = "";
 });
-document.getElementById("addButton").addEventListener("click", (event) => {
-    event.preventDefault();
-    if (document.getElementsByClassName("inputNumber")[0].value != "") {
-        operation[index++] = document.getElementsByClassName("inputNumber")[0].value;
-        operation[index++] = "+";
-        document.getElementsByClassName("inputNumber")[0].value = "";
-        document.getElementsByClassName("inputNumber")[0].focus();
 
-    }
+document.getElementById("addButton").addEventListener("click", (event) => {
+    handleOpClick(event);
 });
+
 document.getElementById("substractButton").addEventListener("click", (event) => {
-    event.preventDefault();
-    if (document.getElementsByClassName("inputNumber")[0].value != "") {
-        operation[index++] = document.getElementsByClassName("inputNumber")[0].value;
-        operation[index++] = "+";
-        document.getElementsByClassName("inputNumber")[0].value = "";
-        document.getElementsByClassName("inputNumber")[0].focus();
-    }
+    handleOpClick(event);
 });
+
 document.getElementById("multiplicationButton").addEventListener("click", (event) => {
-    event.preventDefault();
-    if (document.getElementsByClassName("inputNumber")[0].value != "") {
-        operation[index++] = document.getElementsByClassName("inputNumber")[0].value;
-        operation[index++] = "*";
-        document.getElementsByClassName("inputNumber")[0].value = "";
-        document.getElementsByClassName("inputNumber")[0].focus();
-    }
+    handleOpClick(event);
 });
+
 document.getElementById("divisionButton").addEventListener("click", (event) => {
-    event.preventDefault();
-    if (document.getElementsByClassName("inputNumber")[0].value != "") {
-        operation[index++] = document.getElementsByClassName("inputNumber")[0].value;
-        operation[index++] = "/";
-        document.getElementsByClassName("inputNumber")[0].value = "";
-        document.getElementsByClassName("inputNumber")[0].focus();
+    handleOpClick(event);
+});
+
+document.addEventListener("keydown", (event) => {
+    if (/[0-9]/.test(event.key) || event.key == ".") {
+        if (event.key == "." && document.getElementsByClassName("inputNumber")[0].value == "") {
+            document.getElementsByClassName("inputNumber")[0].value += "0";
+        }
+        document.getElementsByClassName("inputNumber")[0].value += event.key;
+    }
+    //operator control with keys
+    if (event.key == "=" || event.key == "Enter") {
+        document.getElementsByClassName("equalButton")[0].click();
+    }
+    if (event.key == "+") {
+        document.getElementById("addButton").click();
+    }
+    if (event.key == "-") {
+        document.getElementById("substractButton").click();
+    }
+    if (event.key == "*") {
+        document.getElementById("multiplicationButton").click();
+    }
+    if (event.key == "/") {
+        document.getElementById("divisionButton").click();
+    }
+    if (event.key == "Backspace") {
+        if (document.getElementsByClassName("inputNumber")[0].value != "") {
+            const newStr = document.getElementsByClassName("inputNumber")[0].value.slice(0, -1);
+            document.getElementsByClassName("inputNumber")[0].value = newStr;
+        }
     }
 });
